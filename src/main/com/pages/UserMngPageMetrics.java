5b573b27c9f9b72463d0201e390b58a7a9370d58
xinerkeke <943164165@qq.com>
2018-12-26 15:25:31 +0800

    c

--- src/main/com/pages/UserMngPageMetrics.java
+++ src/main/com/pages/UserMngPageMetrics.java
@@ -14,6 +14,7 @@ import org.testng.annotations.Listeners;
 import main.com.base.TestBase01;
 import main.com.util.TestngListener;
 
+
 @Listeners({ TestngListener.class })
 public class UserMngPageMetrics extends TestBase01{
 	
